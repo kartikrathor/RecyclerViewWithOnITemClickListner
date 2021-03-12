3f33b7d9fedc4533d410ec2f7771d226be5a10f2
Kartik rathor <60212068+kartikrathor@users.noreply.github.com>
2021-03-12 17:26:52 +0530

    Create OnSongListener.kt

---
+++ OnSongListener.kt
@@ -0,0 +1,12 @@
+import java.text.FieldPosition
+
+interface onSongItemClickListner{
+
+    fun onSongItemClicked(position: Int)
+
+    fun onTitleClickListner(position: Int)
+
+    fun onDescriptionClickLisener(position: Int)
+
+
+} 
